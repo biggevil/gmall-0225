7b7039f78c289f0117392476d84d31a792eb74e5
solitudiux <1291577591@qq.com>
2021-08-17 02:14:38 +0800

    1

---
+++ untitled/src/com/xxx/he.java
@@ -0,0 +1,12 @@
+package com.xxx;
+
+/**
+ * @author fan
+ * @since 2021-08-17
+ */
+public class he {
+    public static void main(String[] args) {
+        System.out.println("2");
+        System.out.println("1");
+    }
+}
